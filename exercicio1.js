9d812cf2c94f07df0c0b81c9a0ed535fdb482f0b
Filype Abreu <filype.abreu@gmail.com>
2024-12-11 13:42:55 -0300

    Exercicio 1 concluido

--- exercicio1.js
+++ exercicio1.js
@@ -1,5 +1,23 @@
-print("Hello World")
-print("Teste")
-print("Teste Denovo")
-print("Mais um teste")
-print("testando o source control")
+let a = parseInt(prompt('Digite o coeficiente de a:'));
+let b = parseInt(prompt('Digite o coeficiente de b:'));
+let c = parseInt(prompt('Digite o coeficiente de c:'));
+
+let delta = b**2 - (4*a*c);
+
+if (delta < 0) {
+    console.log('Não existem raízes reais');
+} else if (delta > 0) {
+    let raizPositiva = (-b + Math.sqrt(delta)) / (2*a);
+    let raizNegativa = (-b - Math.sqrt(delta)) / (2*a);
+    console.log(`x1 = ${raizPositiva} || x2 = ${raizNegativa}`);
+} else {
+    let raiz = (-b + Math.sqrt(delta)) / (2*a);
+    console.log(`A única raiz é: ${raiz}`);
+}
+
+
+
+
+
+
+
